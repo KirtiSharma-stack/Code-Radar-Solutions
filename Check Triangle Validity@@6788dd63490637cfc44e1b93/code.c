f358f9c0a6754ae74a6f94d51e877cec964922be
KirtiSharma-stack <197675708+KirtiSharma-stack@users.noreply.github.com>
2025-02-17 23:27:58 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,b,c;
+    scanf(%d %d %d,&a,&b,&c);
+    if (c==sqrt(a*a+b*b)){
+        printf("Valid");
+    }
+    else{
+        printf("Invalid");
+    }
+    return 0;
+}
