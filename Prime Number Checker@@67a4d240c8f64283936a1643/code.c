d2960a8097da4d80c5daaa572e6e7beb0bbb68bc
KirtiSharma-stack <197675708+KirtiSharma-stack@users.noreply.github.com>
2025-03-28 14:50:55 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,28 +1,55 @@
-// Your code here...
-// Your code here...
-#include<stdio.h>
-int isPrime(int k){
-    if (k<2){
-        // printf("not prime\n");
-        return 0;
-    }
-    for (int i=2;i*i<k;i++){
-        if (k%i==0){
-            return 0;
-        }
+// // Your code here...
+// // Your code here...
+// #include<stdio.h>
+// int isPrime(int k){
+//     if (k<2){
+//         // printf("not prime\n");
+//         return 0;
+//     }
+//     for (int i=2;i*i<k;i++){
+//         if (k%i==0){
+//             return 0;
+//         }
        
-    }
-    return 1;
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    for (int j=0;j<n;j++){
-        int num;
-        scanf("%d",&num);
-        printf("%d\n",isPrime(num));
+//     }
+//     return 1;
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     for (int j=0;j<n;j++){
+//         int num;
+//         scanf("%d",&num);
+//         printf("%d\n",isPrime(num));
         
+//     }
+
+//     return 0;
+// }
+#include <stdio.h>
+
+int isprime(int k) {
+    if (k < 2) return 0; // Numbers less than 2 are not prime
+
+    for (int i = 2; i * i <= k; i++) {  // Optimized check till sqrt(k)
+        if (k % i == 0) {
+            return 0; // Not prime
+        }
+    }
+    
+    return 1; // Prime
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);  // Read number of test cases
+
+    for (int j = 0; j < n; j++) {
+        int num;
+        scanf("%d", &num);  // Read the number to check
+
+        printf("%d\n", isprime(num));  // Print 1 for prime, 0 for not prime
     }
 
-    return 0;
+    return 0;  // Ensure proper termination
 }
